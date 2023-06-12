650035d85e6d54ca36b64947bd524efe7e4d1510
fahad <99122881+fahad4161@users.noreply.github.com>
2023-06-12 20:02:57 +0600

    class api added

--- index.js
+++ index.js
@@ -30,12 +30,34 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+   
+    const classes = client.db("GlobelSpeck").collection('classes');
+
+    app.get('/classes', async(re1, res) => {
+ 
+      const result = await classes.find().toArray();
+      res.send(result)
+ 
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
